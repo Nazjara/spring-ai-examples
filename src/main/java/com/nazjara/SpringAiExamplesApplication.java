7c6188a683ac4756cc577015d15807100a1019c1
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2024-05-08 18:14:13 +0300

    Add Maven Wrapper and initial project files
    This commit introduces the Maven Wrapper scripts (mvnw and mvnw.cmd) to the project. Additionally, the project's base files including pom.xml, application main class (SpringAiExamplesApplication), and application.properties have been added.
    
    Maven Wrapper ensures that the Maven version used to build the project is consistent across all environments. The other files constitute the basic structure for a Spring Boot application. The initial project settings are defined in the pom.xml, which includes the project's group ID, artifact ID, version, name, and dependencies. The application's entry point is defined in the SpringAiExamplesApplication class. Finally, application properties are placed within the application.properties file.

---
+++ src/main/java/com/nazjara/SpringAiExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.nazjara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiExamplesApplication.class, args);
+	}
+
+}
