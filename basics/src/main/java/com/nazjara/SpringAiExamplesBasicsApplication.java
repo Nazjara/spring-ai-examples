98d6017f14c6f8bb246bfbfcf72d08f8b5e6fcd2
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2024-05-12 19:17:13 +0300

    Renamed application classes and files
    Renamed the SpringAiExamplesApplication to SpringAiExamplesBasicsApplication. Also, created new classes, files, and tests for the prompt-engineering module. Added prompt-engineering as a new module in the pom.xml file.

--- basics/src/main/java/com/nazjara/SpringAiExamplesBasicsApplication.java
+++ basics/src/main/java/com/nazjara/SpringAiExamplesBasicsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiExamplesApplication {
+public class SpringAiExamplesBasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAiExamplesApplication.class, args);
+		SpringApplication.run(SpringAiExamplesBasicsApplication.class, args);
 	}
 
 }
---
+++ prompt-engineering/src/main/java/com/nazjara/SpringAiExamplesPromptsApplication.java
@@ -0,0 +1,12 @@
+package com.nazjara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiExamplesPromptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiExamplesPromptsApplication.class, args);
+	}
+}
