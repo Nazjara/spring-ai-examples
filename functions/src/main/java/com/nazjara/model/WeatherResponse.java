574f7e074034bb4185af72c57b32f66723c9dad3
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2026-09-25 16:09:22 +0300

    Fix snake_case mapping of API Ninjas weather response
    API Ninjas returns wind_speed, min_temp, feels_like, etc., which did not
    bind to the camelCase record fields, so six of ten fields were always
    null. @JsonAlias accepts the snake_case names on read.

--- functions/src/main/java/com/nazjara/model/WeatherResponse.java
+++ functions/src/main/java/com/nazjara/model/WeatherResponse.java
@@ -1,17 +1,18 @@
 package com.nazjara.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import java.math.BigDecimal;
 
-public record WeatherResponse(@JsonPropertyDescription("WindSpeed in KMH") BigDecimal windSpeed,
-                              @JsonPropertyDescription("Direction of wind") Integer windDegrees,
+public record WeatherResponse(@JsonAlias("wind_speed") @JsonPropertyDescription("WindSpeed in KMH") BigDecimal windSpeed,
+                              @JsonAlias("wind_degrees") @JsonPropertyDescription("Direction of wind") Integer windDegrees,
                               @JsonPropertyDescription("Current Temperature in Celsius") Integer temp,
                               @JsonPropertyDescription("Current Humidity") Integer humidity,
                               @JsonPropertyDescription("Epoch time of sunset GMT") Integer sunset,
                               @JsonPropertyDescription("Epoch time of Sunrise GMT") Integer sunrise,
-                              @JsonPropertyDescription("Low Temperature in Celsius") Integer minTemp,
-                              @JsonPropertyDescription("Cloud Coverage Percentage") Integer cloudPct,
-                              @JsonPropertyDescription("Temperature in Celsius") Integer feelsLike,
-                              @JsonPropertyDescription("MaximumTemperature in Celsius") Integer maxTemp) {
+                              @JsonAlias("min_temp") @JsonPropertyDescription("Low Temperature in Celsius") Integer minTemp,
+                              @JsonAlias("cloud_pct") @JsonPropertyDescription("Cloud Coverage Percentage") Integer cloudPct,
+                              @JsonAlias("feels_like") @JsonPropertyDescription("Temperature in Celsius") Integer feelsLike,
+                              @JsonAlias("max_temp") @JsonPropertyDescription("MaximumTemperature in Celsius") Integer maxTemp) {
 }
